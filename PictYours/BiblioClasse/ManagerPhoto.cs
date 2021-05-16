3762d0ac38b40993902557c5bda20c599167c7ac
flmarques1 <flmarques1@304c4609-04f6-4f8a-bacc-098743085384>
2021-05-16 18:37:27 +0000

    Réalisation des documents:  Diagramme de séquence sur deux cas précis  Mise à jour du diagramme de paquetage  Mise à jour du Diagramme de classe Modification de la logique pour la gestion des photos des utilisateurs (A finir !!)

--- PictYours/BiblioClasse/ManagerPhoto.cs
+++ PictYours/BiblioClasse/ManagerPhoto.cs
@@ -8,12 +8,18 @@ namespace BiblioClasse
 {
     public class ManagerPhoto
     {
-        Dictionary<ECategorie, List<Photo>> dicoCategorie;
-        Dictionary<string, List<string>> dicoPhotosAimees;
+        //Dictionary<ECategorie, List<Photo>> dicoCategorie;
+
+        //<Pseudo d'un utilisateur, Liste des identifiants des photos de l'utilisateur>
+        //Dictionary<string, List<Photo>> photosUtilisateurs;
+        internal List<Photo> PhotosUtilisateurs { get; private set; }
+
+        Dictionary<Photo, List<string>> listeUtilisateursParPhotosAimees;
 
         public ManagerPhoto()
         {
-            dicoCategorie = new Dictionary<ECategorie, List<Photo>>();
+            PhotosUtilisateurs = new List<Photo>();
+            listeUtilisateursParPhotosAimees = new Dictionary<Photo, List<string>>();
         }
 
         public void PosterUnePhoto(Utilisateur utilisateur, Photo photo)
@@ -25,14 +31,33 @@ namespace BiblioClasse
             }
         }
 
-        public void SupprimerUnePhoto(List<Utilisateur> listeUtilisateur,Utilisateur utilisateur, string identifiant)
+        /// <summary>
+        /// Supprime une photo pour l'utilisateur passé en paramètre et supprime toutes
+        /// les photos aimées par les autres utilisateurs
+        /// </summary>
+        /// <param name="listeUtilisateurs">La liste de tous les utilisateurs</param>
+        /// <param name="pseudo">Pseudo de l'utilisateur à qui appartient la photo à supprimer</param>
+        /// <param name="identifiant">Identifiant de la photo à supprimer</param>
+        public void SupprimerUnePhoto(List<Utilisateur> listeUtilisateurs,string pseudo, string identifiant)
         {
+            //On recherche l'utilisateur dans la listeUtilisateurs
+            //Utilisateur utilisateur = listeUtilisateurs.Find(u => u.Pseudo.Equals(pseudo));
+            //if (utilisateur == null) return; //S'il n'est pas présent on retourne
+
+            Photo photo = PhotosUtilisateurs.Find(photo => photo.Identifiant.Equals(identifiant));
+            if (photo  == null) return;
+
+            Utilisateur utilisateur = photo.Proprietaire;
+
+            // On vérifie si l'identifiant de la photo passé en paramètre appartient bien à l'utilisateur passé en paramètre
+            //if (!PhotosUtilisateurs.TryGetValue(pseudo, out List<Photo> listePhotoUtilisateur)) return; //Utilisateur présent dans le dico?
+            
+            
             if (utilisateur.EstConnecte)
             {
-                //List<string> listePseudo = dicoPhotosAimees.First(s => s.Equals(identifiant)).Value;
-
-                //listeUtilisateur.Where(u => u.Pseudo.Equals(identifiant));
-
+                //On supprime toutes les occurences de photos dans la liste des photos aimées des autres utilisateurs
+                
+                
                 utilisateur.SupprimerPhoto(identifiant);
                 //Afficher un Dialog en fonction du resultat
             }
