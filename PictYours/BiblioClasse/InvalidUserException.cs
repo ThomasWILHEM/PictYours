a19bcd8dfab21a1011d51a22152d70ce5c550dce
flmarques1 <flmarques1@304c4609-04f6-4f8a-bacc-098743085384>
2021-05-19 15:30:54 +0000

    Ajout de InvalidPhotoException.cs et InvalidUserException.cs pour gérer nos propres exceptions.

---
+++ PictYours/BiblioClasse/InvalidUserException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblioClasse
+{
+    public class InvalidUserException : Exception
+    {
+        public InvalidUserException(string message) : base(message)
+        {
+        }
+
+        public InvalidUserException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
