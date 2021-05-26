ad7631b1fbbf2c62b491bcf91762826447b63946
Thomas WILHEM <81629072+ThomasWILHEM@users.noreply.github.com>
2021-05-26 10:32:35 +0000

    Restructuration de la MainWindow en utilisant un User Control externe

--- PictYours/PictYours/MainWindow.xaml.cs
+++ PictYours/PictYours/MainWindow.xaml.cs
@@ -16,13 +16,13 @@ namespace PictYours
     public partial class MainWindow : Window
     {
         public Manager LeManager => (App.Current as App).LeManager;
-        public ReadOnlyCollection<Utilisateur> ListeUtilisateur { get; private set; }
+        
 
         public MainWindow()
         {
             InitializeComponent();
 
-            ListeUtilisateur = LeManager.ManagerUtilisateur.ListeUtilisateur;
+            
 
             DataContext = this;
         }
@@ -35,17 +35,7 @@ namespace PictYours
         //        MenuDéroulant.Visibility = Visibility.Collapsed;
         //}
 
-        private void ListBoxUtilisateur_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (e.AddedItems.Count == 0)
-            {
-                LeManager.ManagerUtilisateur.UtilisateurSelectionne = LeManager.ManagerUtilisateur.UtilisateurActuel;
-            }
-            else
-            {
-                LeManager.ManagerUtilisateur.UtilisateurSelectionne = e.AddedItems[0] as Utilisateur;
-            }
-        }
+        
 
         private void DeconnexionButton_Click(object sender, RoutedEventArgs e)
         {
@@ -70,41 +60,6 @@ namespace PictYours
             MesParametres.ReinitialiserParametres();
         }
 
-        private void RechercheTextBox_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-            if (NomPrenomRadioButton.IsChecked == true)
-            {
-                if (!RechercheTextBox.Text.Equals(""))
-                {
-                    List<Utilisateur> utilisateursFiltres = RechercheUtilisateur.RechercheParNomEtPrenom(ListeUtilisateur.ToList(), RechercheTextBox.Text);
-                    ListBoxUtilisateur.ItemsSource = utilisateursFiltres;
-                    if (utilisateursFiltres.Count != 0)
-                    {
-                        ListBoxUtilisateur_SelectionChanged(this, new SelectionChangedEventArgs(Selector.SelectionChangedEvent, new List<Utilisateur>(), new List<Utilisateur>() { utilisateursFiltres.First() }));
-                    }
-                }
-                else
-                {
-                    ListBoxUtilisateur.ItemsSource = ListeUtilisateur;
-                }
-
-            }
-            else
-            {
-                List<Utilisateur> utilisateursFiltres = RechercheUtilisateur.RechercheParPseudo(ListeUtilisateur.ToList(), RechercheTextBox.Text);
-                ListBoxUtilisateur.ItemsSource = utilisateursFiltres;
-                if (utilisateursFiltres.Count == 0)
-                {
-                    ListBoxUtilisateur.SelectedItem = LeManager.ManagerUtilisateur.UtilisateurActuel;
-                    //ListBoxUtilisateur_SelectionChanged(this, new SelectionChangedEventArgs(Selector.SelectionChangedEvent, new List<Utilisateur>(), new List<Utilisateur>()));
-                }
-                else
-                {
-                    ListBoxUtilisateur.SelectedItem = utilisateursFiltres.First();
-                    //ListBoxUtilisateur_SelectionChanged(this, new SelectionChangedEventArgs(Selector.SelectionChangedEvent, new List<Utilisateur>(), new List<Utilisateur>() { utilisateursFiltres.First() }));
-                }
-
-            }
-        }
+        
     }
 }
