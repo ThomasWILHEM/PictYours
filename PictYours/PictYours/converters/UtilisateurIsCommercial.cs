80b31c21d9f2772c41c9719d562693b6084701de
flmarques1 <80977545+flomSStaar@users.noreply.github.com>
2021-05-26 13:41:03 +0000

    Ajout d'un converter qui n'est pas utilisé Déplacement de tout les converters dans App.xaml Affichage des photos ItemsControls(UniformGrid) => ListBox

--- PictYours/PictYours/converters/UtilisateurIsCommercial.cs
+++ PictYours/PictYours/converters/UtilisateurIsCommercial.cs
@@ -9,18 +9,13 @@ using System.Windows.Data;
 
 namespace PictYours.converters
 {
-    public class Utilisateur2ModificationProfilButton : IValueConverter
+    class UtilisateurIsCommercial : IValueConverter
     {
-        public Manager LeManager => (App.Current as App).LeManager;
-
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value == null) return "Collapsed";
-            if (value.Equals(LeManager.ManagerUtilisateur.UtilisateurActuel))
-            {
-                return "Visible";
-            }
-            return "Collapsed";
+            if (value == null) return false;
+            if (value is Commercial) return true;
+            return false;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
