5fe3d2fca17215481714af7431145378be8afcb1
Thomas WILHEM <81629072+ThomasWILHEM@users.noreply.github.com>
2021-06-13 12:07:30 +0000

    Suppression et ajout de photo et modification du Stub pour avoir des données "cohérentes" Modification du README.md pour ajouter les identifiants/mots de passe de chaque compte Modification de l'installeur

--- PictYours/Stub/Stub.cs
+++ PictYours/Stub/Stub.cs
@@ -8,10 +8,11 @@ namespace Stub
 {
     public class Stub : IPersistanceManager
     {
-        Amateur pierre = new Amateur("Pierre", "Jean", "pierre.jean", "mdp", "user.png", "Description de Jean Pierre", DateTime.Now.AddYears(-20));
+        Amateur florent = new Amateur("Marques", "Florent", "flomSStaar", "IUT63", "florent.JPG", "Je suis Florent Marques et je suis actuellement à l'IUT informatique de Clermont Ferrand", DateTime.Now.AddYears(-18));
         Amateur tulipe = new Amateur("Tulipe", "Estelle", "estelletulipe", "mdp", "estelle_rond.png", "Description d'Estelle Tulipe", DateTime.Now.AddYears(-15));
-        Amateur thomas = new Amateur("Wilhem", "Thomas", "Atrium", "mdp", "pp.jpg", "Description de Thomas Wilhem", DateTime.Now.AddYears(-18));
-        Commercial mozilla = new Commercial("Mozilla", "mozilla", "mdp", "mozilla.png", "mozilla.fr", "Firefox - le navigateur indépendant soutenu par une organisation à but non lucratif.");
+        Amateur thomas = new Amateur("Wilhem", "Thomas", "Atrium", "IUT63", "thomas.jpg", "Je m'appelle Thomas, j'ai 18 ans et je suis en étide d'Informatique à Clermont Ferrand", new DateTime(2003,02,26));
+        Amateur daniel = new Amateur("Craig", "Daniel", "JamesBond", "007", "danielpp.jpg", "\"Mr Bond, vous avez la fâcheuse habitude de survivre\"", new DateTime(1968, 03, 02));
+        Commercial mozilla = new Commercial("Mozilla", "mozilla", "mieuxquechrome", "mozilla.png", "mozilla.fr", "Firefox - le navigateur indépendant soutenu par une organisation à but non lucratif.");
 
         private string imagesStub = "images_stub";
         private string destImages = "images";
@@ -58,30 +59,38 @@ namespace Stub
 
         private List<Utilisateur> ChargeUtilisateurs()
         {
-            return new List<Utilisateur> { pierre, tulipe, thomas, mozilla };
+            return new List<Utilisateur> { florent, tulipe, thomas, daniel,mozilla };
         }
 
         private Dictionary<Utilisateur, List<Photo>> ChargePhotos()
         {
             Dictionary<Utilisateur, List<Photo>> Dico = new Dictionary<Utilisateur, List<Photo>>();
-            Dico.Add(pierre, new List<Photo> {
-                new Photo("pp.jpg", "test", "test", pierre, DateTime.Today.AddMonths(-6), ECategorie.Automobile),
-                new Photo("vacances6.jpg", "test2", "test1532", pierre, DateTime.Today.AddDays(-5), ECategorie.Animal)
+            Dico.Add(florent, new List<Photo> {
+                new Photo("iut.jpg", "IUT de Clermont Ferrand", "Cezeaux", florent, new DateTime(2020,09,03), ECategorie.Autre),
+                new Photo("uca.png", "Le début d'une aventure", "IUT Informatique Clermont Ferrand", florent, new DateTime(2020,09,03), ECategorie.Autre),
+               
             });
 
             Dico.Add(tulipe, new List<Photo> {
-                new Photo("vacances1.jpg", "test2", "test1532", tulipe, DateTime.Today.AddYears(-1), ECategorie.Sport),
-                new Photo("vacances2.jpg", "test2", "test1532", tulipe, DateTime.Today, ECategorie.Cuisine),
-                new Photo("vacances3.jpg", "test2", "test1532", tulipe, DateTime.Today.AddMonths(-3), ECategorie.Mode)
+                new Photo("maison.jpg", "Ma maison de vacance", "Dans le sud", tulipe, DateTime.Today.AddYears(-1), ECategorie.Autre)               
             });
 
-            Dico.Add(thomas, new List<Photo> { new Photo("montagne.jpg", "test", "test", thomas, DateTime.Today, ECategorie.Animal) });
+            Dico.Add(thomas, new List<Photo> { 
+                new Photo("interstellar.jpg", "L'un des meilleurs films de cette dernière décénie", "Au cinéma", thomas, new DateTime(2016,05,15), ECategorie.Autre),
+                new Photo("astro4.jpg", "Même avec une polution lumineuse, on arrive à de bons résultats", "France", thomas, new DateTime(2017,6,23), ECategorie.Nature),
+                new Photo("astro1.jpg", "Un ciel avec le soleil couchant", "France", thomas, new DateTime(2019,02,13), ECategorie.Nature), 
+                new Photo("astro3.jpg", "Le ciel accompagné par la Voie Lactée", "France", thomas, new DateTime(2020,12,02), ECategorie.Nature),
+                new Photo("astro2.jpg", "Une nuit étoilée", "Du côté de Bordeaux", thomas, new DateTime(2021,05,02), ECategorie.Nature)
+            });
+
+            Dico.Add(daniel, new List<Photo> { 
+                new Photo("voiture.jpg", "Ma voiture d'espion", "Angleterre", daniel, DateTime.Today.AddDays(-560), ECategorie.Automobile),
+                new Photo("bond.jpg", "La fin pour Daniel Craig ?", "Angleterre", daniel, DateTime.Today.AddDays(-25), ECategorie.Autre)
+            });
 
             Dico.Add(mozilla, new List<Photo> {
-                new Photo("vacances4.jpg", "test2", "test1532", mozilla, DateTime.Today.AddMonths(-1), ECategorie.Animal),
-                new Photo("vacances5.jpg", "test2", "test1532", mozilla, DateTime.Today.AddDays(-14), ECategorie.Automobile),
-                new Photo("user.png", "test156", "test123", mozilla, DateTime.Today, ECategorie.Sport),
-                new Photo("estelle_rond.png", "test2", "test1532", mozilla, DateTime.Today, ECategorie.Nature)
+                new Photo("Mozilla_logo.svg.png", "Notre nouveau logo", "Internet", mozilla, DateTime.Today.AddMonths(-1), ECategorie.Autre),
+                new Photo("mozilla-firefox.jpg", "Notre nouvelle charte graphique", "Internet", mozilla, DateTime.Today.AddDays(-15), ECategorie.Autre)
             });
             return Dico;
         }
